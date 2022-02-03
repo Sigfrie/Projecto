828967f7825d76fdfca741074b34630dca0a0fa5
Sigfrie <Magisterflore1s@gmail.com>
2022-02-03 12:36:04 -0500

    cambio

--- project-app/resources/views/welcome.blade.php
+++ project-app/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
+<head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -19,8 +19,10 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
-    </head>
-    <body class="antialiased">
+
+</head>
+
+<body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -128,5 +130,5 @@
                 </div>
             </div>
         </div>
-    </body>
+</body>
 </html>
